6a0a770346ce17c5b11a5eb8f3626d9db9cb5e57
AntonPetrenko <paa2017sh@gmail.com>
2023-01-28 23:34:03 +0200

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const makeListIngredients = ingredients => {
+  return ingredients.map(ingredient => {
+    let item = document.createElement("li");
+    item.textContent = ingredient;
+    item.classList.add('item');
+    return item;
+  });
+};
+  
+const items = makeListIngredients(ingredients);
+
+const listRef = document.querySelector('#ingredients');
+listRef.append(...items);
+
