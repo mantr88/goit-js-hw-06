5fcfc75eb67468e06b2fc6e6eab8f41c1fe4ef8e
AntonPetrenko <paa2017sh@gmail.com>
2023-02-01 07:22:57 +0200

    Task-04 done

--- js/task-04.js
+++ js/task-04.js
@@ -1,18 +1,28 @@
-const refs = {
-    decrement: document.querySelector('button[data-action="decrement"]'),
-    increment: document.querySelector('button[data-action="increment"]'),
-    value: document.querySelector('#value'),
+const decrementBtnRef = document.querySelector('button[data-action="decrement"]');
+const incrementBtnRef = document.querySelector('button[data-action="increment"]');
+const valueRef = document.querySelector('#value');
+
+const counter = {
+    counterValue: 0,
+    increment() {
+        console.log('Кликнули increment');
+        this.counterValue += 1;
+    },
+    decrement() {
+        console.log('Кликнули decrement');
+        this.counterValue -= 1;
+    },
 };
 
-let counterValue = 0;
-
-refs.decrement.addEventListener('click', () => {
-    return counterValue -= 1;
+decrementBtnRef.addEventListener('click', () => {
+    counter.decrement();
+    valueRef.textContent = counter.counterValue;
 });
-refs.increment.addEventListener('click', () => {
-    return counterValue += 1;
+incrementBtnRef.addEventListener('click', () => {
+    counter.increment();
+    valueRef.textContent = counter.counterValue;
 });
 
-console.log(counterValue);
-refs.value.textContent = counterValue;
+
+
 
