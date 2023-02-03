eda6d1c5ec14b8eb9224992f55b5fd5aaf8ce891
AntonPetrenko <paa2017sh@gmail.com>
2023-02-03 21:53:33 +0200

    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formRef = document.querySelector('.login-form');
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const elements = event.currentTarget.elements;
+    const email = elements.email.value;
+    const password = elements.password.value;
+
+    if (email !== '' && password !== '') {
+        console.log({ email, password });
+        formRef.reset();
+    } else {
+        alert('Увага! Всі поля повинні бути заповнені!');
+    }
+};
 
