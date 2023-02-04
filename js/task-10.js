2ae004afdcac983ecf025029cc6060207095b4e0
AntonPetrenko <paa2017sh@gmail.com>
2023-02-04 21:11:01 +0200

    task-10 did non finish part2

--- js/task-10.js
+++ js/task-10.js
@@ -9,28 +9,58 @@ const refs = {
   boxesDiv: document.querySelector('#boxes'),
 }
 
-
-let amount = 0;
-// console.log(refs.input)
-
-refs.input.addEventListener('input', receivesAmount);
-// refs.createBtn.addEventListener('click', createBoxes);
-
-
 function receivesAmount(event) {
-  // console.log(Number(event.currentTarget.value))
-  amount = Number(event.currentTarget.value);
+  let amount = event.currentTarget.value;
+  
+  createBoxes(amount)
+  }
+
+  
+let array = [];
+  
+function createBoxes(amount) {
+  const totalAmount = array.length + amount;
+  for (let i = array.length; i < totalAmount; i += 1) {
+    array.push(document.createElement('div'));    
+  }
+      
 }
+      
+console.log(array)
+      
+      refs.input.addEventListener('input', receivesAmount);
+      // // refs.createBtn.addEventListener('click', );
+      
 
-console.log(amount);
 
-// let array = [];
+// Напиши скрипт створення і очищення колекції елементів. 
+// Користувач вводить кількість елементів в input і натискає кнопку Створити,
+// після чого рендериться колекція.Натисненням на кнопку Очистити, колекція елементів очищається.
 
-// function createBoxes(amount) {
-//   for (let i = 0; i < amount; i+= 1) {
-//     array.push(document.createElement('div'));
-    
-//   }
-//   return console.log(array)
+// Створи функцію createBoxes(amount), яка приймає один параметр - число.Функція створює стільки < div >,
+//   скільки вказано в amount і додає їх у div#boxes.
+
+// 1. Розміри найпершого <div> - 30px на 30px.
+// 2. Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// 3. Всі елементи повинні мати випадковий колір фону у форматі HEX.Використовуй готову функцію getRandomHexColor 
+// для отримання кольору.
+
+
+// const onInputClick = event => {
+//   const newDiv = refs.input.value;
+  
+//   console.log(newDiv)
+//   // createBoxes(newDiv);
 // }
+
+// refs.input.addEventListener('input', onInputClick);
+
+
+
+
+
+
+
+
+
 
