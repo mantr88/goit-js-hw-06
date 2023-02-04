51a2dd157b55173e7dc9075adf31531adb3f525a
AntonPetrenko <paa2017sh@gmail.com>
2023-02-04 07:20:39 +0200

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRef = document.querySelector('.change-color');
+const printColorRef = document.querySelector('.color');
+const bodyRef = document.querySelector('body');
+
+btnRef.addEventListener('click', changeColorBgrBody)
+
+function changeColorBgrBody(event) {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  printColorRef.textContent = getRandomHexColor();
+}
