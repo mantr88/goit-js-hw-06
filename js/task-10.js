60160211fcac2febb9aeef6b23c3016257aa7d33
AntonPetrenko <paa2017sh@gmail.com>
2023-02-04 20:09:10 +0200

    task-10 did not finish

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector('input'),
+  createBtn: document.querySelector('button[data-create]'),
+  destroyBtn: document.querySelector('button[data-destroy]'),
+  boxesDiv: document.querySelector('#boxes'),
+}
+
+
+let amount = 0;
+// console.log(refs.input)
+
+refs.input.addEventListener('input', receivesAmount);
+// refs.createBtn.addEventListener('click', createBoxes);
+
+
+function receivesAmount(event) {
+  // console.log(Number(event.currentTarget.value))
+  amount = Number(event.currentTarget.value);
+}
+
+console.log(amount);
+
+// let array = [];
+
+// function createBoxes(amount) {
+//   for (let i = 0; i < amount; i+= 1) {
+//     array.push(document.createElement('div'));
+    
+//   }
+//   return console.log(array)
+// }
+
